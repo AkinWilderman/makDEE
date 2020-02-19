886748f0bce008c38b80dfb966ca2de60a6491e1
Akin Wilderman <akindavid84@gmail.com>
2020-02-19 01:20:49 +0100

    minor edit

--- src/main/Main.js
+++ src/main/Main.js
@@ -23,85 +23,10 @@ const MakdeeSpot = () => {
             style={{height: 'auto', width: 90}}
           />
           <View style={{marginLeft: '5%'}}>
-            <Text style={{fontSize: 18, flex: 1}}>
-              {' '}
-              Roasted Meat Burger{' '}
-            </Text>
+            <Text style={{fontSize: 18, flex: 1}}> Roasted Meat Burger </Text>
             <Text style={{fontWeight: 'bold'}}> Starting from ₦200 </Text>
             <Text> sold 550, free delivery </Text>
             <Text> with cabbage and spices (optional) </Text>
-          </View>
-        </View>
-      </Card>
-      <Card>
-        <View style={styles.cardItemView}>
-          <Image
-            source={require('../media/roasted_chicken.jpeg')}
-            style={{height: 'auto', width: 90}}
-          />
-          <View style={{marginLeft: '5%'}}>
-            <Text style={{fontSize: 18, flex: 1}}>
-              Roasted Chicken Burger d
-            </Text>
-            <Text style={{fontWeight: 'bold'}}>Starting from ₦400</Text>
-            <Text>sold 500, free delivery</Text>
-            <Text>with cabbage and spices (optional)</Text>
-          </View>
-        </View>
-      </Card>
-      <Card>
-        <View style={styles.cardItemView}>
-          <Image
-            source={require('../media/roasted_fish.jpeg')}
-            style={{height: 'auto', width: 90}}
-          />
-          <View style={{marginLeft: '5%'}}>
-            <Text style={{fontSize: 18, flex: 1}}>Roasted Fish Burger</Text>
-            <Text style={{fontWeight: 'bold'}}>Starting from ₦300</Text>
-            <Text>sold 360, free delivery</Text>
-            <Text>with cabbage and spices (optional)</Text>
-          </View>
-        </View>
-      </Card>
-      <Card>
-        <View style={styles.cardItemView}>
-          <Image
-            source={require('../media/fried_fish.jpeg')}
-            style={{height: 'auto', width: 90}}
-          />
-          <View style={{marginLeft: '5%'}}>
-            <Text style={{fontSize: 18, flex: 1}}>Fried Fish Burger</Text>
-            <Text style={{fontWeight: 'bold'}}>Starting from ₦250</Text>
-            <Text>sold 180, free delivery</Text>
-            <Text>with cabbage and spices (optional)</Text>
-          </View>
-        </View>
-      </Card>
-      <Card>
-        <View style={styles.cardItemView}>
-          <Image
-            source={require('../media/fried_meat.jpeg')}
-            style={{height: 'auto', width: 90}}
-          />
-          <View style={{marginLeft: '5%'}}>
-            <Text style={{fontSize: 18, flex: 1}}>Fried Meat Burger</Text>
-            <Text style={{fontWeight: 'bold'}}>Starting from ₦200</Text>
-            <Text>sold 220, free delivery</Text>
-            <Text>with cabbage and spices (optional)</Text>
-          </View>
-        </View>
-      </Card>
-      <Card>
-        <View style={styles.cardItemView}>
-          <Image
-            source={require('../media/fried_chicken.jpeg')}
-            style={{height: 'auto', width: 90}}
-          />
-          <View style={{marginLeft: '5%'}}>
-            <Text style={{fontSize: 18, flex: 1}}>Fried Chicken Burger</Text>
-            <Text style={{fontWeight: 'bold'}}>Starting from ₦400</Text>
-            <Text>sold 500, free delivery</Text>
-            <Text>with cabbage and spices (optional)</Text>
           </View>
         </View>
       </Card>
@@ -121,81 +46,6 @@ const OtherSpots = () => {
           />
           <View style={{marginLeft: '5%'}}>
             <Text> Suleiman Spot</Text>
-            <Text style={{fontSize: 18, flex: 1}}>Roasted Meat Burger</Text>
-            <Text style={{fontWeight: 'bold'}}>Starting from ₦150</Text>
-            <Text>delivery ₦50</Text>
-            <Text>with cabbage and spices (optional)</Text>
-          </View>
-        </View>
-      </Card>
-      <Card>
-        <View style={styles.cardItemView}>
-          <Image
-            source={require('../media/suya.jpg')}
-            style={{height: 'auto', width: 90}}
-          />
-          <View style={{marginLeft: '5%'}}>
-            <Text> Social center Spot</Text>
-            <Text style={{fontSize: 18, flex: 1}}>Roasted Meat Burger</Text>
-            <Text style={{fontWeight: 'bold'}}>Starting from ₦150</Text>
-            <Text>delivery ₦50</Text>
-            <Text>with cabbage and spices (optional)</Text>
-          </View>
-        </View>
-      </Card>
-      <Card>
-        <View style={styles.cardItemView}>
-          <Image
-            source={require('../media/suya.jpg')}
-            style={{height: 'auto', width: 90}}
-          />
-          <View style={{marginLeft: '5%'}}>
-            <Text> Nineties Spot</Text>
-            <Text style={{fontSize: 18, flex: 1}}>Roasted Meat Burger</Text>
-            <Text style={{fontWeight: 'bold'}}>Starting from ₦150</Text>
-            <Text>delivery ₦50</Text>
-            <Text>with cabbage and spices (optional)</Text>
-          </View>
-        </View>
-      </Card>
-      <Card>
-        <View style={styles.cardItemView}>
-          <Image
-            source={require('../media/suya.jpg')}
-            style={{height: 'auto', width: 90}}
-          />
-          <View style={{marginLeft: '5%'}}>
-            <Text> FSLT Spot</Text>
-            <Text style={{fontSize: 18, flex: 1}}>Roasted Meat Burger</Text>
-            <Text style={{fontWeight: 'bold'}}>Starting from ₦150</Text>
-            <Text>delivery ₦50</Text>
-            <Text>with cabbage and spices (optional)</Text>
-          </View>
-        </View>
-      </Card>
-      <Card>
-        <View style={styles.cardItemView}>
-          <Image
-            source={require('../media/suya.jpg')}
-            style={{height: 'auto', width: 90}}
-          />
-          <View style={{marginLeft: '5%'}}>
-            <Text> ICSA/RAMAT Spot</Text>
-            <Text style={{fontSize: 18, flex: 1}}>Roasted Meat Burger</Text>
-            <Text style={{fontWeight: 'bold'}}>Starting from ₦150</Text>
-            <Text>delivery ₦50</Text>
-            <Text>with cabbage and spices (optional)</Text>
-          </View>
-        </View>
-      </Card>
-      <Card>
-        <View style={styles.cardItemView}>
-          <Image
-            source={require('../media/suya.jpg')}
-            style={{height: 'auto', width: 90}}
-          />
-          <View style={{marginLeft: '5%'}}>
-            <Text> Education Spot</Text>
             <Text style={{fontSize: 18, flex: 1}}>Roasted Meat Burger</Text>
             <Text style={{fontWeight: 'bold'}}>Starting from ₦150</Text>
             <Text>delivery ₦50</Text>
